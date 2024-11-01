4d9f7195b67e026210dd7955b6592a7f58f04b45
valentina-mut <159520924+valentina-mut@users.noreply.github.com>
2024-11-01 19:30:43 +0300

    Add files via upload

---
+++ Practice_2/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите ваше имя: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Привет, " + name + ".");
+
+        scanner.close();
+    }
+}
